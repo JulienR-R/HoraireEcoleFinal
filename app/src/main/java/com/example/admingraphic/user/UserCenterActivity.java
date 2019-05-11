9fa95fe9e8b9f2560ded89420cff0ed4006081c4
kikemtl <48916544+kikemtl@users.noreply.github.com>
2019-05-11 16:42:05 -0400

    Add files via upload

--- app/src/main/java/com/example/admingraphic/user/UserCenterActivity.java
+++ app/src/main/java/com/example/admingraphic/user/UserCenterActivity.java
@@ -1,9 +1,8 @@
-package com.example.admingraphic.user;
+package com.example.horaire.user;
 
-import android.content.Context;
+
 import android.content.DialogInterface;
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
@@ -14,7 +13,8 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
-import com.example.admingraphic.R;
+import com.example.horaire.R;
+
 
 /**
  * Created by 201663676 on 2019-04-23.
@@ -50,14 +50,21 @@ public class UserCenterActivity extends AppCompatActivity implements View.OnClic
                 break;
 
             case R.id.user_horairechoice:
-            //vérifier que le choix d'horaire est activé
+                //vérifier que le choix d'horaire est activé
                 Intent intentHoraireChoice = new Intent(this, HoraireChoiceActivity.class);
                 startActivity(intentHoraireChoice);
-            break;
+                break;
 
             case R.id.user_admincontact:
-            // passer à une application externe de courriel
-             break;
+               Intent intent = new Intent(Intent.ACTION_SEND);
+               intent.setType("text/plain");
+                intent.putExtra(Intent.EXTRA_EMAIL, "manon_robert-rochon@gmail.com");
+                intent.putExtra(Intent.EXTRA_SUBJECT, "Problèmes/Informations");
+                intent.putExtra(Intent.EXTRA_TEXT, "Écrivez votre message ici.");
+
+                startActivity(Intent.createChooser(intent, "Send Email"));
+
+                break;
         }
     }
 
@@ -94,11 +101,7 @@ public class UserCenterActivity extends AppCompatActivity implements View.OnClic
                 .setNegativeButton(R.string.btnCancel, null)
                 .setPositiveButton(R.string.btnConfirm, new DialogInterface.OnClickListener() {
                     public void onClick(DialogInterface arg0, int arg1) {
-                        SharedPreferences sharedPreferences = getSharedPreferences("MyPreferences", Context.MODE_PRIVATE);
-                        SharedPreferences.Editor editor = sharedPreferences.edit();
-                        editor.putBoolean("stayConnected",false);
-                        editor.apply();
-                        finish();
+                        // connection key down and move to loginpage
                     }
                 }).create().show();
     }
@@ -108,14 +111,14 @@ public class UserCenterActivity extends AppCompatActivity implements View.OnClic
         new AlertDialog.Builder(this)
                 .setTitle(R.string.title_edit_password)
                 .setMessage(R.string.message_edit_password)
-            .setNegativeButton(R.string.btnCancel, null)
-            .setView(newpassword)
-            .setPositiveButton(R.string.btnConfirm, new DialogInterface.OnClickListener() {
-                public void onClick(DialogInterface arg0, int arg1) {
-                    // si password n'est pas le même qu'actuel et qu'il n'est pas vide
-                    // changer password dans la base de donnée
-                    // sinon, toast expliquant l'erreur (Tu as oublié de mettre un mot de passe)
-                }
-            }).create().show();
+                .setNegativeButton(R.string.btnCancel, null)
+                .setView(newpassword)
+                .setPositiveButton(R.string.btnConfirm, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface arg0, int arg1) {
+                        // si password n'est pas le même qu'actuel et qu'il n'est pas vide
+                        // changer password dans la base de donnée
+                        // sinon, toast expliquant l'erreur (Tu as oublié de mettre un mot de passe)
+                    }
+                }).create().show();
     }
 }
