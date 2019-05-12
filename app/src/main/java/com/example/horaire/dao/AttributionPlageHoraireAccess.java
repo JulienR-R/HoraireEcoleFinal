34a6c7eb2764d34fee6bbd6456d2d663ce32f5cb
chouags <49240361+chouags@users.noreply.github.com>
2019-05-12 19:38:20 -0400

    Add files via upload

---
+++ app/src/main/java/com/example/horaire/dao/AttributionPlageHoraireAccess.java
@@ -0,0 +1,36 @@
+package com.example.horaire.dao;
+
+import android.arch.persistence.room.Dao;
+import android.arch.persistence.room.Delete;
+import android.arch.persistence.room.Insert;
+import android.arch.persistence.room.OnConflictStrategy;
+import android.arch.persistence.room.Query;
+import android.arch.persistence.room.Update;
+
+import com.example.horaire.database.AttributionPlageHoraire;
+
+import java.util.List;
+
+@Dao
+public interface AttributionPlageHoraireAccess {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    long insertAttributionPlageHoraire(AttributionPlageHoraire attributionPlageHoraire);
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    void insertAttributionPlageHoraires(List<AttributionPlageHoraire> attributionPlageHoraires);
+
+    @Query("SELECT * FROM attributionPlageHoraire")
+    List<AttributionPlageHoraire> getAttributionPlageHoraires();
+
+    @Query("SELECT * FROM attributionPlageHoraire WHERE _id = :attributionPlageHoraire")
+    AttributionPlageHoraire getAttributionPlageHoraire(int attributionPlageHoraire);
+
+    @Query("SELECT * FROM attributionPlageHoraire WHERE idChoixPlageHoraire = :idChoixPlageHoraire")
+    AttributionPlageHoraire getAttributionPlageHoraireByChoix(int idChoixPlageHoraire);
+
+    @Update()
+    int updateAttributionPlageHoraire(AttributionPlageHoraire attributionPlageHoraire);
+
+    @Delete
+    int deleteAttributionPlageHoraire(AttributionPlageHoraire attributionPlageHoraire);
+}
---
+++ app/src/main/java/com/example/horaire/dao/UserAccess.java
@@ -0,0 +1,33 @@
+package com.example.horaire.dao;
+
+import android.arch.persistence.room.Dao;
+import android.arch.persistence.room.Delete;
+import android.arch.persistence.room.Insert;
+import android.arch.persistence.room.OnConflictStrategy;
+import android.arch.persistence.room.Query;
+import android.arch.persistence.room.Update;
+
+import com.example.horaire.database.AttributionPlageHoraire;
+import com.example.horaire.database.User;
+
+import java.util.List;
+
+@Dao
+public interface UserAccess {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    long insertUser(User user);
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    void insertUsers(List<User> users);
+    @Query("SELECT * FROM users WHERE isAdmin = 0")
+    List<User> getUsers();
+    @Query("SELECT * FROM users WHERE _id = :id")
+    User getUser(int id);
+    @Query("SELECT * FROM users WHERE userID = :userId AND pwd = :pwd")
+    User getUser(String userId, String pwd);
+    @Query("SELECT * FROM attributionPlageHoraire WHERE ")
+    List<AttributionPlageHoraire> getUserAttributions(int idUser);
+    @Update()
+    int updateUser(User user1);
+    @Delete
+    int deleteUser(User user);
+}
